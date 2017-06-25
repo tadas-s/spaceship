1256a4ae7d5926764ea985921d86cca365ebd4c9
Tadas Sasnauskas <tadas@refme.com>
2017-06-25 13:52:35 +0100

    Use abelectronics ADCPi to read analog inputs

--- spaceship/input/analog_in.py
+++ spaceship/input/analog_in.py
@@ -1,6 +1,7 @@
 from ..base_process import BaseProcess
 from time import sleep
-from random import randrange
+from ADCPi.ABE_helpers import ABEHelpers
+from ADCPi.ABE_ADCPi import ADCPi
 
 
 class AnalogIn(BaseProcess):
@@ -9,10 +10,33 @@ class AnalogIn(BaseProcess):
         super(AnalogIn, self).__init__(logger=logger, quit_flag=quit_flag)
 
     def run(self):
-        dummy_1 = 0.0
+        i2c_helper = ABEHelpers()
+        bus = i2c_helper.get_smbus()
+
+        if not bus:
+            self.logger.warn('No i2c bus? Quitting.')
+            return
+
+        adc = ADCPi(bus, 0x6e, 0x6f, 12)
+
+        voltages = [
+            adc.read_voltage(1),
+            adc.read_voltage(2),
+            adc.read_voltage(3),
+            adc.read_voltage(4),
+            adc.read_voltage(5),
+            adc.read_voltage(6),
+            adc.read_voltage(7),
+            adc.read_voltage(8)
+        ]
 
         while not self.quit():
-            dummy_1 += randrange(1, 30) / 1000
-            dummy_1 = dummy_1 % 100
-            self.outgoing.put(('analog_1', randrange(1, 100)))
-            sleep(0.1)
+            for port in range(0, 8):
+                voltage = adc.read_voltage(port + 1)
+
+                if voltages[port] != voltage:
+                    voltages[port] = voltage
+                    self.outgoing.put(('analog_%i' % port, voltage))
+                    self.logger.debug('analog_%i=%f' % (port, voltage))
+
+            sleep(0.02)
