30edb5ab11b3ba033fabcb4cc8662a714594d33f
Tadas Sasnauskas <tadas@yoyo.lt>
2017-06-29 23:28:49 +0100

    Mildly ugly/sketchy code to read all 32 digital in ports

--- spaceship/input/digital_in.py
+++ spaceship/input/digital_in.py
@@ -17,26 +17,42 @@ class DigitalIn(BaseProcess):
             self.logger.warn('No i2c bus? Quitting.')
             return
 
-        digital_in = IoPi(bus, 0x20)
-        digital_in.set_port_direction(0, 0xFF)
-        digital_in.set_port_direction(1, 0xFF)
-        digital_in.set_port_pullups(0, 0xFF)
-        digital_in.set_port_pullups(1, 0xFF)
+        bus1 = IoPi(bus, 0x20)
+        bus1.set_port_direction(0, 0xFF)
+        bus1.set_port_direction(1, 0xFF)
+        bus1.set_port_pullups(0, 0xFF)
+        bus1.set_port_pullups(1, 0xFF)
+
+        bus2 = IoPi(bus, 0x21)
+        bus2.set_port_direction(0, 0xFF)
+        bus2.set_port_direction(1, 0xFF)
+        bus2.set_port_pullups(0, 0xFF)
+        bus2.set_port_pullups(1, 0xFF)
 
         # set initial values to something unusual to trigger
         # readout messages on startup
         pins = [
+            None, None, None, None, None, None, None, None,
+            None, None, None, None, None, None, None, None,
             None, None, None, None, None, None, None, None,
             None, None, None, None, None, None, None, None
         ]
 
         while not self.quit():
             for port in range(0, 16):
-                pin = digital_in.read_pin(port + 1)
+                pin = bus1.read_pin(port + 1)
 
                 if pins[port] != pin:
                     pins[port] = pin
                     self.outgoing.put(('digital_%i' % port, pin))
                     self.logger.debug('digital_%i=%i' % (port, pin))
+
+            for port in range(0, 16):
+                pin = bus2.read_pin(port + 1)
+
+                if pins[port + 16] != pin:
+                    pins[port + 16] = pin
+                    self.outgoing.put(('digital_%i' % (port + 16), pin))
+                    self.logger.debug('digital_%i=%i' % ((port + 16), pin))
 
             sleep(0.1)
